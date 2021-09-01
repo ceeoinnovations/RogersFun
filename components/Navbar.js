5b2b991b808e2b5659285a865d4808d8016d03e0
chrisbuerginrogers <crogers@tufts.edu>
2021-08-31 21:29:53 -0400

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar(about, items){
         <ul>
            ${about==='project'? (
                 `<li class="nav-title">
-                    <a href="/product-template">← Go Back</a>
+                    <a href="/RogersFun">← Go Back</a>
                 </li>`
            ):(
             `
